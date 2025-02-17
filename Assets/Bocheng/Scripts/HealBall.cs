fd4fef7854e8a8a47fba78315f7ba336ed0c9291
BoCheng Huang <bc_huang@foxmail.com>
2025-02-16 21:10:55 -0800

    merge

--- Assets/Bocheng/Scripts/HealBall.cs
+++ Assets/Bocheng/Scripts/HealBall.cs
@@ -2,27 +2,27 @@
 
 public class HealBall : MonoBehaviour
 {
-    public float healAmount = 10f; // 治疗量
+    public float healAmount = 5f; // 治疗量
 
     void OnTriggerEnter2D(Collider2D other)
     {
 
         // 只对目标 Tag 触发
-        if (other.CompareTag("Player"))
-        {
-            return;
-        }
-        else
+        if (other.CompareTag("Player") && other.CompareTag("Enemy") && other.CompareTag("Teammate"))
         {
             DoSomething(other.gameObject);
-
-            Destroy(gameObject);
         }
+
+        Destroy(gameObject);
     }
 
     // **执行治疗或其他行为**
     void DoSomething(GameObject target)
     {
-        // Do something here
+        Health health = target.GetComponent<Health>();
+        if (health != null)
+        {
+            health.Heal(healAmount);
+        }
     }
 }
