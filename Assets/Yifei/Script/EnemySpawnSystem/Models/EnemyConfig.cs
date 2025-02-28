77ea71fa75c4b4c8af9eb4a32f93f0e9e91a94a8
Syoinn <Syoinn@outlook.com>
2025-02-28 01:50:26 -0800

    加入敌人列表和敌人配置
    待测试

---
+++ Assets/Yifei/Script/EnemySpawnSystem/Models/EnemyConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemies/Enemy")]
+public class EnemyConfig : ScriptableObject
+{
+    // 需要怪物脚本中有相应的初始化方法
+    [Header("基础属性")]
+    public string displayName = "New Enemy";
+    [Range(1, 100)] public int baseHealth = 10;
+    [Range(1, 20)] public int baseDamage = 2;
+    [Range(0.1f, 10f)] public float moveSpeed = 3f;
+
+    [Header("难度设置")]
+    [Tooltip("该敌人出现的难度阈值")]
+    [Range(0, 10)] public float difficultyThreshold = 1f;
+    [Tooltip("在相同难度下的生成权重")]
+    public float spawnWeight = 1f;
+
+    [Header("战斗系统")]
+    public AttackPattern attackPattern;
+    public List<Cast> skills = new List<Cast>();
+
+    [Header("预制体")]
+    public GameObject prefab;
+}
+
+public enum AttackPattern
+{
+    // 不一定用得上
+    Melee,
+    Ranged,
+}
+
+[System.Serializable]
+public class Cast
+{
+    // 不一定用得上
+    public string skillName;
+    public float cooldown;
+    public GameObject[] effectPrefabs;
+}
---
+++ Assets/Yifei/Script/EnemySpawnSystem/Models/EnemyLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemies/Enemy Library")]
+public class GlobalEnemyLibrary : ScriptableObject
+{
+    public List<EnemyConfig> allEnemies = new List<EnemyConfig>();
+
+    // 可视化排序按钮
+    public void SortByDifficulty()
+    {
+        allEnemies.Sort((a, b) =>
+            a.difficultyThreshold.CompareTo(b.difficultyThreshold));
+    }
+}
