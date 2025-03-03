41d37f5beaefedb485e3004b2c4f7c639449cd98
BoCheng Huang <bc_huang@foxmail.com>
2025-03-02 22:28:41 -0800

    Init trader

---
+++ Assets/Bocheng/Scripts/Trader/Trader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Trader : MonoBehaviour
+{
+    public Canvas worldSpaceCanvas;  // 商人的交互提示 Canvas
+    public GameObject shopUI;        // 商店UI
+    private bool isPlayerNear = false;
+
+    private void Start()
+    {
+        worldSpaceCanvas.gameObject.SetActive(false);
+        shopUI.SetActive(false);
+    }
+
+    public void PlayerEnteredRange()
+    {
+        isPlayerNear = true;
+        worldSpaceCanvas.gameObject.SetActive(true);
+    }
+
+    public void PlayerExitedRange()
+    {
+        isPlayerNear = false;
+        worldSpaceCanvas.gameObject.SetActive(false);
+        shopUI.SetActive(false); // 关闭商店UI
+    }
+
+    private void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleShop();
+        }
+    }
+
+    private void ToggleShop()
+    {
+        shopUI.SetActive(!shopUI.activeSelf);
+    }
+}
