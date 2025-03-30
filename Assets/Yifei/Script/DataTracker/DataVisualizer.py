9671b835fcbf63eb929b7ca945359606ebcf9124
Syoinn <Syoinn@outlook.com>
2025-03-30 16:02:43 -0700

    Import tutorial asset

--- Assets/Yifei/Script/DataTracker/DataVisualizer.py
+++ Assets/Yifei/Script/DataTracker/DataVisualizer.py
@@ -13,7 +13,15 @@ levels = [1, 2, 3, 4, 5]
 counts = {level: 0 for level in levels}
 
 for session_id, session_data in data.items():
-    if "a" in session_data and "DifficultyLevelReached" in session_data["a"]: # for WebGL build
+    if "a" in session_data and "DifficultyLevelReached" in session_data["a"]:
+        play_time_str = session_data["a"].get("PlayTime", "0")
+        try:
+            play_time = float(play_time_str)
+        except ValueError:
+            continue
+        if play_time < 20:
+            continue
+
         level_str = session_data["a"].get("DifficultyLevelReached", "0")
         try:
             level = int(level_str)
